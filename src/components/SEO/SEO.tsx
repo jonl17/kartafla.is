b3d68933c2fdef04082acb43bf8e9f20367b745f
Jón Gabríel Lorange <jonl17@ru.is>
2022-01-07 11:47:23 +0000

    favicon

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -1,10 +1,11 @@
-import React from "react"
-import { Helmet } from "react-helmet"
+import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const SEO = () => {
   return (
     <Helmet>
       <title>kartafla</title>
+      <link rel='favicon' href='/favicon.ico' />
     </Helmet>
   )
 }
